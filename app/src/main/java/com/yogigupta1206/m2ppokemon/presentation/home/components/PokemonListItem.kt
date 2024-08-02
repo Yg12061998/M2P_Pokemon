5f2b6446c77abe669526a9191ef9445ca360b886
Yogesh Gupta <yogesh1.gupta@paytm.com>
2024-08-03 01:59:53 +0530

    PokemonListItem fixed

--- app/src/main/java/com/yogigupta1206/m2ppokemon/presentation/home/components/PokemonListItem.kt
+++ app/src/main/java/com/yogigupta1206/m2ppokemon/presentation/home/components/PokemonListItem.kt
@@ -1,6 +1,5 @@
 package com.yogigupta1206.m2ppokemon.presentation.home.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,18 +11,21 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.request.CachePolicy
 import coil.request.ImageRequest
+import coil.transform.RoundedCornersTransformation
 import com.yogigupta1206.m2ppokemon.R
+import com.yogigupta1206.m2ppokemon.domain.model.ImageUrls
 import com.yogigupta1206.m2ppokemon.domain.model.PokemonCardPreview
 import kotlinx.coroutines.Dispatchers
 
@@ -34,67 +36,49 @@ fun PokemonListItem(pokemon: PokemonCardPreview, onClick: () -> Unit) {
             .fillMaxWidth()
             .padding(8.dp)
             .clickable { onClick() },
-        shape = RoundedCornerShape(8.dp)
+        shape = RoundedCornerShape(14.dp)
     ) {
         Row(
-            modifier = Modifier
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
+            modifier = Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically
 
         ) {
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(pokemon.images.small)
-                .crossfade(true)
-                .dispatcher(Dispatchers.IO)
-                .build(),
-            contentDescription = pokemon.name,
-        modifier = Modifier.size(80.dp),
-        contentScale = ContentScale.Crop
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Column {
-            Text(text = pokemon.name)
-            Row(modifier = Modifier.padding(top = 4.dp)) {
-                pokemon.types?.forEach { type ->
-                    Box(
-                        modifier = Modifier
-                            .padding(end = 4.dp)
-                            /*.background(
-                                Color(
-                                    android.graphics.Color.parseColor(
-                                        "#${type.color}"
-                                    )
-                                ),
-                                shape = RoundedCornerShape(4.dp)
-                            )*/
-                            .padding(4.dp),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(text = type, color = Color.White)
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(pokemon.images.small)
+                    .crossfade(true)
+                    .dispatcher(Dispatchers.IO)
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .error(R.drawable.ic_launcher_background)
+                    .transformations(RoundedCornersTransformation())
+                    .networkCachePolicy(CachePolicy.ENABLED)
+                    .build(),
+                contentDescription = pokemon.name,
+                modifier = Modifier.size(80.dp),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(text = pokemon.name, style = MaterialTheme.typography.titleLarge)
+                Row(modifier = Modifier.padding(top = 4.dp)) {
+                    pokemon.types?.forEach { type ->
+                        Box(
+                            modifier = Modifier
+                                .padding(end = 8.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = type, style = MaterialTheme.typography.titleSmall)
+                        }
                     }
                 }
-            }
-            Row(
-                modifier = Modifier.padding(top = 4.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-
-                contentDescription = "Level",
-                modifier = Modifier.size(16.dp)
-                )
-                Text(text = "Lvl ${pokemon.level}", modifier = Modifier.padding(start = 4.dp))
-                Spacer(modifier = Modifier.weight(1f))
-                Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = "HP",
-                    modifier = Modifier.size(16.dp)
-                )
-                Text(text = "${pokemon.hp} HP", modifier = Modifier.padding(start = 4.dp))
+                Row(
+                    modifier = Modifier.padding(top = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "Lvl ${pokemon.level}", style = MaterialTheme.typography.labelLarge)
+                    Spacer(modifier = Modifier.weight(1f))
+                    Text(text = "${pokemon.hp} HP", modifier = Modifier.padding(start = 4.dp),style = MaterialTheme.typography.labelLarge)
+                }
             }
         }
-    }
     }
 }
